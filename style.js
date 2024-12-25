a6cb6baddae9facae4d8dd39f5a0279f892f815c
Kha <Kha999888777666@gmail.com>
2024-12-25 08:37:52 +0700

    Kha

--- style.js
+++ style.js
@@ -13,3 +13,14 @@ document.getElementById('envelope').addEventListener('click', function() {
         card.classList.remove('show');
     }, 2000); // Thời gian đợi 2 giây
 });
+
+window.addEventListener("resize", () => {
+    const width = window.innerWidth;
+    const card = document.getElementById("card");
+
+    if (width <= 600) {
+        card.style.transform = "scale(0.8)";
+    } else {
+        card.style.transform = "scale(1)";
+    }
+});
